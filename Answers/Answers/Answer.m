422a5afc3519a557f6db48f1912a7f66abe2a7f8
Teunozz <t.klijn@student.fontys.nl>
2013-12-02 22:36:03 +0100

    Revert "Changed to new service implementation"
    This reverts commit 39da3f4fd4f0ffb45985af3a7407dfc070c39a86.

--- Answers/Answers/Answer.m
+++ Answers/Answers/Answer.m
@@ -12,12 +12,11 @@
 
 /**
  * Sends a answer with the WebserviceManager SharedInstance.
- * tested with local service waiting for real implementation
  * @param parameters: parameters for the service
  */
 + (NSURLSessionDataTask *) postAnswerWithParameters:(NSDictionary*) parameters withBlock: (void (^)(NSError *error))block
 {
-    return [[WebserviceManager sharedClient] POST:@"AnswerService.svc/answers" parameters:parameters
+    return [[WebserviceManager sharedClient] POST:@"CreateAnswer" parameters:parameters
             success:^(NSURLSessionDataTask *task, id responseObject) {
                 if (block) {
                     block(nil);
