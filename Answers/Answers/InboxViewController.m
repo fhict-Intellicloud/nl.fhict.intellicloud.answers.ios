1c87fb1e4dc55d503c296a1c66f65c49c6407201
ferdiduisters <ferdiduisters@gmail.com>
2013-12-05 19:08:43 +0100

    Added custom UITableViewCell class

--- Answers/Answers/InboxViewController.m
+++ Answers/Answers/InboxViewController.m
@@ -13,25 +13,13 @@
 
 @implementation InboxViewController
 
-@synthesize inboxTableView;
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     self.title = NSLocalizedString(@"Inbox", nil);
     
-    
-    //Binds CustomCellView.xib to tableview
-    //Each viewcontroller with a tableview needs this snip-it
-    UINib *cellNIB = [UINib nibWithNibName:@"CustomCellView" bundle:nil];
-    if (cellNIB)
-    {
-        [inboxTableView registerNib:cellNIB forCellReuseIdentifier:@"CustomCell"];
-        [inboxTableView setRowHeight: 86];
-
-    } else NSLog(@"failed to load nib");
-    //End binding
+    [self.inboxTableView setRowHeight:86];
 }
 
 - (void)didReceiveMemoryWarning
@@ -56,50 +44,16 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
     static NSString *CellIdentifier = @"CustomCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    
-    UILabel *inboxNameLabel = (UILabel *)[cell.contentView viewWithTag:301];
-    UIImageView *inboxImage = (UIImageView *)[cell.contentView viewWithTag:302];
-    UILabel *inboxTimeLabel = (UILabel *)[cell.contentView viewWithTag:303];
-    UILabel *inboxQuestionLabel = (UILabel *)[cell.contentView viewWithTag:304];
-    
-    
-    
-    switch(indexPath.row)
-    {
-        case 0:
-            inboxImage.image = [UIImage imageNamed:@"MenuIconOpen"];
-            inboxNameLabel.text = @"Paul PHP Poos";
-            inboxTimeLabel.text = @"3 minuten geleden";
-            inboxQuestionLabel.text = @"Hoe stel ik internet in?";
-            break;
-        case 1:
-            inboxImage.image = [UIImage imageNamed:@"MenuIconPending"];
-            inboxNameLabel.text = @"Lex Mastergenie";
-            inboxTimeLabel.text = @"3 minuten geleden";
-            inboxQuestionLabel.text = @"Hoe maak ik een Xcode project aan?";
-            break;
-        case 2:
-            inboxImage.image = [UIImage imageNamed:@"MenuIconReview"];
-            inboxNameLabel.text = @"Gadget Ferdi";
-            inboxTimeLabel.text = @"3 minuten geleden";
-            inboxQuestionLabel.text = @"Hoe voeg ik Sencha Touch toe aan Phonegap?";
-            break;
-        case 3:
-            inboxImage.image = [UIImage imageNamed:@"MenuIconRejected"];
-            inboxNameLabel.text = @"Bram iOS Kersten";
-            inboxTimeLabel.text = @"3 minuten geleden";
-            inboxQuestionLabel.text = @"Android Imageview wilt niet lukken?";
-            break;
-        default:
-            inboxImage.image = [UIImage imageNamed:@"MenuIconRejected"];
-            inboxNameLabel.text = @"Some user";
-            inboxTimeLabel.text = @"Some X minutes";
-            inboxQuestionLabel.text = @"Some question";
-            break;
+    CustomTableCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = (CustomTableCell *)[CustomTableCell cellFromNibNamed:@"CustomCellView"];
     }
+    
+    cell.state = stateRejected;
+    cell.LabelName.text = @"Paul PHP Poos";
+    cell.LabelTime.text = @"3 minuten geleden";
+    cell.LabelQuestion.text = @"Hoe stel ik internet in?";
     
     return cell;
 }
