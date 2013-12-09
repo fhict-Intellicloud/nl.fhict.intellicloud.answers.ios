04242017c43bcb361d6c5be85cfa5d0b198d445b
bramkerstenmedia <Bram@MacBook-Air-van-Bram.local>
2013-12-09 12:10:14 +0100

    Review Detail Viewcontroller UI finished

--- Answers/ReviewDetailViewController.h
+++ Answers/ReviewDetailViewController.h
@@ -11,16 +11,34 @@
 @interface ReviewDetailViewController : UIViewController
 
 /**
+ * @property questionLabel
+ * @brief Label for the question
+ */
+@property (strong, nonatomic) IBOutlet UILabel *questionLabel;
+
+/**
  * @property questionTextView
  * @brief Textview that holds the question to be shown
  */
 @property (strong, nonatomic) IBOutlet UITextView *questionTextView;
 
 /**
+ * @property answerColleagueLabel
+ * @brief Label for colleague answer
+ */
+@property (strong, nonatomic) IBOutlet UILabel *answerColleagueLabel;
+
+/**
  * @property answerColleagueTableView
  * @brief Tableview that holds one or more answer(s) of colleague
  */
 @property (strong, nonatomic) IBOutlet UITableView *answerColleagueTableView;
+
+/**
+ * @property commentsLabel
+ * @brief Label for comments
+ */
+@property (strong, nonatomic) IBOutlet UILabel *commentsLabel;
 
 /**
  * @property commentsTextView
@@ -33,5 +51,11 @@
  * @brief Label with the comments placeholder
  */
 @property (strong, nonatomic) IBOutlet UILabel *commentsPlaceholderLabel;
+
+/**
+ * @property sendReviewButton
+ * @brief Button for sending the review
+ */
+@property (strong, nonatomic) IBOutlet UIButton *sendReviewButton;
 
 @end
